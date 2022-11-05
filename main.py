6e580c8497a4358a569557e4edb56b94f32425a2
Przemysław Talarek <przemyslaw.talarek@gmail.com>
2022-11-05 12:10:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
-import pandas as pd
-import numpy as np
+import sys
+import subprocess
+import pkg_resources
 
-def func1(a, b):
-    print(f'{a} was first argument, {b} was second argument.')
+def libraries_dependecies_check():
+    """
+    Check if needed libraries are installed. These are:
+        - Numpy
+        - Pandas
+    """
+    installed_packages = pkg_resources.working_set
+    installed_packages_key = sorted([f'{i.key}' for i in installed_packages])
+    libraries_pool = ['pandas', 'numpy', 'scipy', 'torch']
 
+    for lib in libraries_pool:
+        if lib in installed_packages_key:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', '--upgrade', lib])
+        else:
+            subprocess.check_call([sys.executable, '-m', 'pip', 'install', lib])
+            
 
-func1('alfa', 'beta')
+libraries_dependecies_check()
